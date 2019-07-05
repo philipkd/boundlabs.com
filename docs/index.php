491aac1370b86b8fac90b83c23914cf57f4a495b
Philip Dhingra <philipkd@gmail.com>
2019-07-04 18:53:04 -0700

    now swaps out gallery from text files

--- docs/index.php
+++ docs/index.php
@@ -11,7 +11,9 @@ $contents = str_replace("</style>",'</style><link rel="stylesheet" href="custom.
 $contents = str_replace('$description-nohtml',"Research-to-Development Lab in San Francisco, CA",$contents);
 $contents = str_replace('$description-html','Research-to-Development Lab<br/>San Francisco, CA<br/><a href="mailto:phil@boundlabs.com">phil@boundlabs.com</a>',$contents);
 
-$contents = str_replace('$bitcoin',article('bitcoin'),$contents);
+$articles = article('permanote') . article('whack');
+
+$contents = str_replace('$articles',$articles,$contents);
 
 // $contents = str_replace('<script src="http://static.tumblr.com/vr9xgox/2VWmqthak/main-min.js"></script>', '', $contents);
 
