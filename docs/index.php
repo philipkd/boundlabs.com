9df0c30606a2d377878c9464aa42b2602fc73660
Philip Dhingra <philipkd@gmail.com>
2019-07-04 18:39:14 -0700

    got rid of extraneous html

--- docs/index.php
+++ docs/index.php
@@ -9,7 +9,7 @@ $contents = file_get_contents('source.html');
 $contents = str_replace("</style>",'</style><link rel="stylesheet" href="custom.css">', $contents);
 
 $contents = str_replace('$description-nohtml',"Research-to-Development Lab in San Francisco, CA",$contents);
-$contents = str_replace('$description-html','Research-to-Developmen Lab<br/>San Francisco, CA<br/><a href="mailto:phil@boundlabs.com">phil@boundlabs.com</a>',$contents);
+$contents = str_replace('$description-html','Research-to-Development Lab<br/>San Francisco, CA<br/><a href="mailto:phil@boundlabs.com">phil@boundlabs.com</a>',$contents);
 
 $contents = str_replace('$bitcoin',article('bitcoin'),$contents);
 
