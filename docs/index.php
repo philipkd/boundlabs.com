36611656312c124f37b0bfc7632a4a0b68d2cdd6
Philip Dhingra <philipkd@gmail.com>
2019-07-04 18:36:47 -0700

    now it capture an article from a store

--- docs/index.php
+++ docs/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('error_reporting',E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
+include_once "markdown.php";
+
 $contents = file_get_contents('source.html');
 
 $contents = str_replace("</style>",'</style><link rel="stylesheet" href="custom.css">', $contents);
@@ -7,9 +11,14 @@ $contents = str_replace("</style>",'</style><link rel="stylesheet" href="custom.
 $contents = str_replace('$description-nohtml',"Research-to-Development Lab in San Francisco, CA",$contents);
 $contents = str_replace('$description-html','Research-to-Developmen Lab<br/>San Francisco, CA<br/><a href="mailto:phil@boundlabs.com">phil@boundlabs.com</a>',$contents);
 
+$contents = str_replace('$bitcoin',article('bitcoin'),$contents);
 
 // $contents = str_replace('<script src="http://static.tumblr.com/vr9xgox/2VWmqthak/main-min.js"></script>', '', $contents);
 
 print $contents;
+
+function article($name) {
+	return file_get_contents("contents/$name.txt");
+}
 
 ?>
