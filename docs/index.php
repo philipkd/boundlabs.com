104e6332edb38e57dd64bedb91b2c25df1986b1f
Philip Dhingra <philipkd@gmail.com>
2019-07-04 18:29:42 -0700

    Now has description and words substituted

--- docs/index.php
+++ docs/index.php
@@ -4,6 +4,10 @@ $contents = file_get_contents('source.html');
 
 $contents = str_replace("</style>",'</style><link rel="stylesheet" href="custom.css">', $contents);
 
+$contents = str_replace('$description-nohtml',"Research-to-Development Lab in San Francisco, CA",$contents);
+$contents = str_replace('$description-html','Research-to-Developmen Lab<br/>San Francisco, CA<br/><a href="mailto:phil@boundlabs.com">phil@boundlabs.com</a>',$contents);
+
+
 // $contents = str_replace('<script src="http://static.tumblr.com/vr9xgox/2VWmqthak/main-min.js"></script>', '', $contents);
 
 print $contents;
